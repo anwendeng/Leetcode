516836006a155e3cfa350fb1f808e6fb2362e439
anwendeng <31466347+anwendeng@users.noreply.github.com>
2025-08-19 17:50:11 +0800

    Time: 59 ms (22.72%), Space: 28.5 MB (42.33%) - LeetHub

--- 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.py
+++ 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.py
@@ -1,4 +1,4 @@
 class Solution:
     def zeroFilledSubarray(self, nums: List[int]) -> int:
-        return (Len:=0) or sum( Len:=Len+1 if x==0 else 0 for x in nums )
+        return (Len:=0) or sum( Len:=Len+1 & -(x==0) for x in nums )
           
