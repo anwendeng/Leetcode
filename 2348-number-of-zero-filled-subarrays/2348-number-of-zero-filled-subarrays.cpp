ce5214ae371b5c456dd34be44ba70fcaac6aa8c3
anwendeng <31466347+anwendeng@users.noreply.github.com>
2025-08-19 17:56:57 +0800

    Time: 0 ms (100%), Space: 111.3 MB (40.61%) - LeetHub

--- 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.cpp
+++ 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.cpp
@@ -3,4 +3,4 @@ public:
     long long zeroFilledSubarray(vector<int>& nums) { 
         return accumulate(nums.begin(), nums.end() , 0LL, 
 [&, len=0](long long sum, int x) mutable {
-            return sum+=(len=(x!=0)?0:len+1);});}};
+            return sum+=(len=len+1 & -(x==0));});}};
